a63527a9badc759db1d65a487c8eb887470397c5
Robert Brook <robertbrook@fastmail.fm>
2024-01-16 11:45:40 +0000

    Create stepdefs.rb

---
+++ features/step_definitions/stepdefs.rb
@@ -0,0 +1,11 @@
+Given("today is Sunday") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I ask whether it's Friday yet") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be told {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
