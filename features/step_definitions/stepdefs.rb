cd1549001a6a6e4853d2c66ae75cc89515957e93
Robert Brook <robertbrook@fastmail.fm>
2024-02-08 11:58:35 +0000

    Update stepdefs.rb

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -93,10 +93,10 @@ Then('the reponse should contain the search term {string}') do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given('I have typed in the query: {string}') do |string|
+Given('I have typed in the query: {string}$') do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When('the Solr query is: {string}') do |string|
+When('the Solr query is: {string}$') do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
