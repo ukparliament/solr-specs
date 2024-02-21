86a83d1a53561913c51b67f7e604f8effc4f1df9
fantasticlife <michael@smethur.st:wq>
2024-02-21 14:28:54 +0000

    completed tests for basic solr features

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'nokogiri'
 
 
 Given( 'I can connect to the web' ) do
@@ -33,28 +34,17 @@ end
 
 When('I send a query to the Prod Solr API with the params:') do |table|
   
-  # We get the API key for Production Solr.
-  api_key = ENV['API_KEY']
-  
   # We extract the query string from the data table.
   query_string = table.raw[0][1]
   
+  # We extract the row count from the data table.
+  row_count = table.raw[1][1]
+  
   # We construct the URL for Production Solr.
-  url = "https://api.parliament.uk/solr?q=#{query_string}"
+  url = "https://api.parliament.uk/solr?q=#{query_string}&rows=#{row_count}"
   
-  # We URIify the URL.
-  uri = URI( url )
-  
-  # We construct the request ...
-  request = Net::HTTP::Get.new( uri )
-  
-  # ... and append the API key.
-  request['Ocp-Apim-Subscription-Key'] = api_key
-  
-  # We get the response.
-  @response = Net::HTTP.start( uri.hostname, uri.port, use_ssl: uri.scheme == 'https' ) { |http|
-    http.request( request )
-  }
+  # We get the response from the Solr XML.
+  @response = get_response( url )
 end
 
 When('I send a query to the Test Solr API with the params:') do |table|
@@ -65,8 +55,46 @@ When('I send a query to the Test Solr API with the params:') do |table|
   # We extract the query string from the data table.
   query_string = table.raw[0][1]
   
+  # We extract the row count from the data table.
+  row_count = table.raw[1][1]
+  
   # We construct the URL for Test Solr.
-  url = "https://api.parliament.uk/new-solr/select?q=#{query_string}"
+  url = "https://api.parliament.uk/new-solr/select?q=#{query_string}&rows=#{row_count}&wt=xml"
+  
+  # We get the response from the Solr XML.
+  @response = get_response( url )
+end
+
+Then( 'the API should return an HTTP response code of {string}' ) do |string|
+  
+  # We check the response code.
+  expect( @response.code ).to eq string
+end
+
+And( 'the response should have {int} rows' ) do |int|
+  
+  # We get the document count from the response XML.
+  document_count = get_document_count_from_xml( @response.body )
+  
+  # We check the result count.
+  expect( document_count ).to eq int
+end
+
+And( 'the response should contain the search term {string}' ) do |string|
+  
+  # We get the query string from the response XML.
+  query_string = get_query_string_from_xml( @response.body )
+  
+  # We check the response code.
+  expect( query_string ).to eq string
+end
+
+
+# A method to get the response of the Solr XML.
+def get_response( url )
+  
+  # We get the API key for Test and Production Solr.
+  api_key = ENV['API_KEY']
   
   # We URIify the URL.
   uri = URI( url )
@@ -78,42 +106,28 @@ When('I send a query to the Test Solr API with the params:') do |table|
   request['Ocp-Apim-Subscription-Key'] = api_key
   
   # We get the response.
-  @response = Net::HTTP.start( uri.hostname, uri.port, use_ssl: uri.scheme == 'https' ) { |http|
+  Net::HTTP.start( uri.hostname, uri.port, use_ssl: uri.scheme == 'https' ) { |http|
     http.request( request )
   }
 end
 
-Then( 'the API should return an HTTP response code of {string}' ) do |string|
+
+# ## A method to get the document count from the response body of the Solr XML.
+def get_document_count_from_xml( response_body )
   
-  # We check the response code.
-  expect( @response.code ).to eq string
+  # We get the Solr response body as XML.
+  doc  = Nokogiri::XML( response_body)
+  
+  # We get a count of the number of doc elements returned from the XML.
+  doc.xpath( '//doc' ).count
 end
 
-
-
-
-
-
-
-
-
-Then('the HTTP response should be {int}') do |int|
-# Then('the HTTP response should be {float}') do |float|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('the response should have seven rows') do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then('the reponse should contain the search term {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given(/I have typed in the query: (.*)$/) do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/the Solr query is: (.*)$/) do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+# ## A method to get the query string from the response body of the Solr XML.
+def get_query_string_from_xml( response_body )
+  
+  # We get the Solr response body as XML from the XML.
+  doc  = Nokogiri::XML( response_body)
+  
+  # We get a count of the query string.
+  doc.xpath( "response/lst[@name='responseHeader']/lst[@name='params']/str[@name='q']/text()" ).to_s
 end
