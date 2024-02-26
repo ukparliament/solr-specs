854cb732e6bad09f1124a7ebef916e3ecd8322e6
Robert Brook <robertbrook@fastmail.fm>
2024-02-26 17:33:24 +0000

    a bit more odin

--- features/support/odin.rb
+++ features/support/odin.rb
@@ -1,16 +1,47 @@
+
+
 module Odin
+	
 	class Formatter
+	
 	  def initialize(config)
 		@io = config.out_stream
-		config.on_event :test_case_started do |event|
-		  print_test_case_name(event.test_case)
-		end
-	  end
-	
-	  def print_test_case_name(test_case)
-	    @io.puts "  #{test_case.name.upcase}"
+		
+		config.on_event :test_case_finished do |event|
+			@io.puts 
 
-            
+			status = "UNKNOWN"
+		  	case event.result.class.to_s
+			when "Cucumber::Core::Test::Result::Passed"
+			  status = "Passed"
+			when "Cucumber::Core::Test::Result::Failed"
+			  status = "\e[1mFAILED\e[22m"
+			when "Cucumber::Core::Test::Result::Skipped"
+			  status = "\e[3mSkipped\e[23m"
+			else
+			  status = event.result.class.to_s
+			end
+			
+			@io.puts "#{status}\t#{event.test_case.name}"
+		end
+		
+		config.on_event :test_step_finished do |event|
+			
+			status = "UNKNOWN"
+		  	case event.result.class.to_s
+			when "Cucumber::Core::Test::Result::Passed"
+			  status = "Passed"
+			when "Cucumber::Core::Test::Result::Failed"
+			  status = "\e[1mFAILED\e[22m"
+			when "Cucumber::Core::Test::Result::Skipped"
+			  status = "\e[3mSkipped\e[23m"
+			else
+			  status = event.result.class.to_s
+			end
+			
+			@io.puts "\t#{status}\t#{event.test_step}"
+		end
+		
 	  end
 	end
 end
