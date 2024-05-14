2faa9e52b78124a6df05cd2ef7d44f242dec375f
fantasticlife <michael@smethur.st:wq>
2024-05-14 16:58:05 +0100

    s parameter now sort

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -44,7 +44,7 @@ When('I send a query to the Prod Solr API with the params:') do |table|
   row_count = table.raw[1][1]
   
   # We construct the URL for Production Solr.
-  url = "#{PRODUCTION_SOLR_URL}?q=#{query_string}&rows=#{row_count}&s=date"
+  url = "#{PRODUCTION_SOLR_URL}?q=#{query_string}&rows=#{row_count}&sort=date"
   
   # We get the response from the Solr XML.
   @response = get_response( url )
@@ -59,7 +59,7 @@ When('I send a query to the Test Solr API with the params:') do |table|
   row_count = table.raw[1][1]
   
   # We construct the URL for Test Solr.
-  url = "#{TEST_SOLR_URL}&q=#{query_string}&rows=#{row_count}&s=date"
+  url = "#{TEST_SOLR_URL}&q=#{query_string}&rows=#{row_count}&sort=date"
   
   # We get the response from the Solr XML.
   @response = get_response( url )
@@ -74,13 +74,13 @@ When('I send a query to the Test and Production Solr APIs with the params:') do 
   row_count = table.raw[1][1]
   
   # We construct the URL for Test Solr.
-  test_url = "#{TEST_SOLR_URL}&q=#{query_string}&rows=#{row_count}&s=date"
+  test_url = "#{TEST_SOLR_URL}&q=#{query_string}&rows=#{row_count}&sort=date"
   
   # We get the response from the Test Solr XML.
   @test_response = get_response( test_url )
   
   # We construct the URL for Production Solr.
-  production_url = "#{PRODUCTION_SOLR_URL}?q=#{query_string}&rows=#{row_count}&s=date"
+  production_url = "#{PRODUCTION_SOLR_URL}?q=#{query_string}&rows=#{row_count}&sort=date"
   
   # We get the response from the Production Solr XML.
   @production_response = get_response( production_url )
